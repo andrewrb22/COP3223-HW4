25f0dcd195a340c0e8b491715b8efc305a9b1f24
Andrew Rodriguez <andrewrb22@gmail.com>
2022-02-11 18:47:58 -0500

    done testing for HW4

--- HW4.c
+++ HW4.c
@@ -1,36 +1,85 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+// Division process
+void division(int n)
 {
-    int number, i;
-    char again;
-    int firstNumber = 0;
-    do
+    if (n < 1)
+    {
+        printf("%d not a natural number\n", n);
+    }
+    else
     {
 
-        printf("Enter a natural number: ");
-        scanf("%d", &number);
-
-        for (i = 1; i <= 10; i++)
+        printf("\ndivisors of %d are:", n);
+        for (int i = 1; i <= n; i++)
         {
-            printf("%d x %d = %d\n", number, i, number * i);
-        };
-
+            if (n % i == 0)
+                printf("%d ", i);
+        }
+    }
+}
+// Multiplication  process
+void multiplication(int n)
+{
+    if (n < 1)
+    {
+        printf("%d is not a natural number", n);
+    }
+    else
+    {
+        int range, i = 1;
         do
         {
-            if (firstNumber != 0)
-                printf("\n Sorry invalid character... Options are:  Y/N or y/n \n");
+            printf("Enter the range: ");
+            scanf("%d", &range);
+        } while (range <= 0);
+        while (i <= range)
+        {
+            printf("%d * %d = %d \n", i, n, n * i);
+            ++i;
+        }
+    }
+}
 
-            printf("\nDo you want to try a new number (Y/N): ");
-            scanf(" %c", &again);
-            firstNumber = 1;
-
-        } while (again != 'Y' && again != 'y' && again != 'n' && again != 'N');
-
-    } while (again == 'Y' || again == 'y');
-
-    printf("\nThank you for using the program");
-
+int main()
+{
+    char options;
+    int n;
+    printf("\n\tChoose from the following options: \n\n");
+    printf("1) A or a:  To display the divisors of a natural number\n");
+    printf("2) B or b: To display the multiplication table for a natural number\n");
+    printf("3) Q or q to Exit\n");
+    printf("\nEnter your choice: ");
+    scanf("%c", &options);
+    if (options != 'A' && options != 'a' && options != 'b' && options != 'B')
+    {
+        printf("%c is not a valid option", options);
+    }
+    // Selecting an options
+    else
+    {
+        switch (options)
+        {
+        case 'a':
+        case 'A':
+            printf("Enter your number:");
+            scanf("%d", &n);
+            division(n);
+            printf("\nThank you for using the program, Bye");
+            break;
+        case 'b':
+        case 'B':
+            printf("Enter your number:");
+            scanf("%d", &n);
+            multiplication(n);
+            printf("\nThank you for using the program, Bye");
+            break;
+        case 'q':
+        case 'Q':
+            printf("\nThank you for using the program, Bye");
+            break;
+        }
+    }
     return 0;
 }
